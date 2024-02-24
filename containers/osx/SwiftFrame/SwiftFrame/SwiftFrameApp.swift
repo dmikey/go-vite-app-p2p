bca2d954e64c06163d29ba2ec474749adc5332c1
Derek Anderson <dmikey@users.noreply.github.com>
2024-02-24 12:47:24 -0600

    add a container shell for OSX
    Signed-off-by: Derek Anderson <dmikey@users.noreply.github.com>

---
+++ containers/osx/SwiftFrame/SwiftFrame/SwiftFrameApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftFrameApp.swift
+//  SwiftFrame
+//
+//  Created by Derek Anderson on 2/24/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftFrameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
