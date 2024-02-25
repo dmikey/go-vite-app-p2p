9ab97f81ba99d9079d4635245c884e5e9810a615
Derek Anderson <dmikey@users.noreply.github.com>
2024-02-25 00:07:55 -0600

    more complete demo layout
    Signed-off-by: Derek Anderson <dmikey@users.noreply.github.com>

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,20 +1,97 @@
-import { useState } from 'react'
-import { Button } from "@/components/ui/button"
-import { Input } from "@/components/ui/input"
+import { useState } from "react";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
 
-import './App.css'
+import {
+	Menubar,
+	MenubarContent,
+	MenubarItem,
+	MenubarMenu,
+	MenubarSeparator,
+	MenubarShortcut,
+	MenubarTrigger,
+} from "@/components/ui/menubar";
+
+import {
+	Card,
+	CardContent,
+	CardDescription,
+	CardFooter,
+	CardHeader,
+	CardTitle,
+} from "@/components/ui/card";
+
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+
+import "./App.css";
+import { Home } from "lucide-react";
 
 function App() {
-  const [count, setCount] = useState(0)
+	const [count, setCount] = useState(0);
 
-  return (
-    <>
-      <div>
-        <Input />
-        <Button>Click me</Button>
-      </div>
-    </>
-  )
+	return (
+		<>
+			<div className="app-container">
+				<div className="t">
+					<Menubar style={{ paddingTop: 25, paddingBottom: 25 }}>
+						<MenubarMenu>
+							<MenubarTrigger>
+								<Avatar>
+									<AvatarImage src="https://github.com/shadcn.png" />
+									<AvatarFallback>CN</AvatarFallback>
+								</Avatar>
+							</MenubarTrigger>
+						</MenubarMenu>
+						<MenubarMenu>
+							<MenubarTrigger>File</MenubarTrigger>
+							<MenubarContent>
+								<MenubarItem>
+									New Tab <MenubarShortcut>⌘T</MenubarShortcut>
+								</MenubarItem>
+								<MenubarItem>New Window</MenubarItem>
+								<MenubarSeparator />
+								<MenubarItem>Share</MenubarItem>
+								<MenubarSeparator />
+								<MenubarItem>Print</MenubarItem>
+							</MenubarContent>
+						</MenubarMenu>
+						<MenubarMenu>
+							<MenubarTrigger>Help</MenubarTrigger>
+							<MenubarContent>
+								<MenubarItem>About</MenubarItem>
+							</MenubarContent>
+						</MenubarMenu>
+					</Menubar>
+				</div>
+				<div className="l">
+					<Button
+						variant="ghost"
+						style={{
+							width: "100%",
+							textAlign: "left",
+							justifyContent: "start",
+						}}
+					>
+						Welcome
+					</Button>
+				</div>
+				<div className="r" style={{ padding: "20px" }}>
+					<Card>
+						<CardHeader>
+							<CardTitle>Card Title</CardTitle>
+							<CardDescription>Card Description</CardDescription>
+						</CardHeader>
+						<CardContent>
+							<p>Card Content</p>
+						</CardContent>
+						<CardFooter>
+							<p>Card Footer</p>
+						</CardFooter>
+					</Card>
+				</div>
+			</div>
+		</>
+	);
 }
 
-export default App
+export default App;
